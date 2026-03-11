c757dd775d19bd6813465e5b0a419835e43fe070
Oleksander  Shevchenko <oleksandr.shevchenko@damen.com>
2026-03-11 15:05:20 +0200

    Add simple dashboard for viewing tasks as unordered list for current user - add template and view for it. Add url as start page.

---
+++ work_time_reporter/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'work_time_reporter'
+
+urlpatterns = [
+    # When the user enters the main page of the application, we call views.dashboard
+    path('', views.dashboard, name='dashboard'),
+]
--- work_time_reporter/views.py
+++ work_time_reporter/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Task
 
-# Create your views here.
+
+@login_required(login_url='/admin/login/')  # temporary use login from admin panel
+def dashboard(request):
+    # Searching for tasks assigned to current user (current user is in assignees list)
+    user_tasks = Task.objects.filter(assignees=request.user).select_related('project')
+
+    context = {
+        'tasks': user_tasks
+    }
+    return render(request, 'work_time_reporter/dashboard.html', context)
