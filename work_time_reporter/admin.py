eff52f8d4b74deb651e6404232b4208f1ce7779e
Oleksander  Shevchenko <oleksandr.shevchenko@damen.com>
2026-03-11 15:06:13 +0200

    Add horizontal filter for  selection assigned users for task and project.

--- work_time_reporter/admin.py
+++ work_time_reporter/admin.py
@@ -9,12 +9,16 @@ class ProjectAdmin(admin.ModelAdmin):
     list_filter = ('project_type', 'is_active', 'year')
     # Search field by name
     search_fields = ('name',)
+    # add convenient interface for selecting user
+    filter_horizontal = ('members',)
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('title', 'project', 'budget_hours', 'deadline', 'status')
     list_filter = ('status', 'project')
     search_fields = ('title',)
+    # add convenient interface for selecting user
+    filter_horizontal = ('assignees',)
 
 @admin.register(TimeLog)
 class TimeLogAdmin(admin.ModelAdmin):
