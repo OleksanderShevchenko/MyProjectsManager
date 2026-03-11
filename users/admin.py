96b341097438d446bcdb8867231e87de1a8cbf40
Oleksander  Shevchenko <oleksandr.shevchenko@damen.com>
2026-03-11 12:11:39 +0200

    register user name in admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+# Register our custom user model in admin panel, using standard view UserAdmin
+admin.site.register(CustomUser, UserAdmin)
