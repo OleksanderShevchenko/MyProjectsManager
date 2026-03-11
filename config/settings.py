48b0bbe5292d83d5de39b6fa676a154f524fe528
Oleksander  Shevchenko <oleksandr.shevchenko@damen.com>
2026-03-11 12:25:27 +0200

    Add new application

--- config/settings.py
+++ config/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # first local app
-    'users'
+    'users',
+    'work_time_reporter',
 ]
 
 MIDDLEWARE = [
---
+++ work_time_reporter/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WorkTimeReporterConfig(AppConfig):
+    name = 'work_time_reporter'
