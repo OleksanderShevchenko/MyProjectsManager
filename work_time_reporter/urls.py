5c732f2546c9ae34d01ac37ea56f37a749997231
Oleksander  Shevchenko <oleksandr.shevchenko@damen.com>
2026-03-17 17:37:30 +0200

    Add urls for login and logout

--- work_time_reporter/urls.py
+++ work_time_reporter/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'work_time_reporter'
 
 urlpatterns = [
-    # When the user enters the main page of the application, we call views.dashboard
+    # urls for a user lon-in / log-out
+    path('login/', auth_views.LoginView.as_view(template_name='work_time_reporter/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='work_time_reporter:login'), name='logout'),
+
+    # When the user enters the main page of the application, we call views.dashboard - it redirect us to current week
     path('', views.dashboard, name='dashboard'),
     path('<int:year>/<int:week>/', views.dashboard, name='dashboard_week'),
+
 ]
