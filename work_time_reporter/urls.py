6370b1ecf14739ff12d133ad23ca7d23520840c7
Oleksander  Shevchenko <oleksandr.shevchenko@damen.com>
2026-03-18 09:40:25 +0200

    Add url for manager's cabinet

--- work_time_reporter/urls.py
+++ work_time_reporter/urls.py
@@ -9,6 +9,9 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='work_time_reporter/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='work_time_reporter:login'), name='logout'),
 
+    # Manager's cabinet
+    path('approvals/', views.team_approvals, name='team_approvals'),
+
     # When the user enters the main page of the application, we call views.dashboard - it redirect us to current week
     path('', views.dashboard, name='dashboard'),
     path('<int:year>/<int:week>/', views.dashboard, name='dashboard_week'),
