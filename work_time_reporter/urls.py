68b3f479c2e1da3eca47f7b06a59387c34e7d458
Oleksander  Shevchenko <oleksandr.shevchenko@damen.com>
2026-03-18 12:00:26 +0200

    Add url to use new view and template to display other user week report details for his/her manager.

--- work_time_reporter/urls.py
+++ work_time_reporter/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
 
     # Manager's cabinet
     path('approvals/', views.team_approvals, name='team_approvals'),
+    # root to view details of week report for approval (in read-only mode)
+    path('timesheet/<int:timesheet_id>/', views.timesheet_detail, name='timesheet_detail'),
 
     # When the user enters the main page of the application, we call views.dashboard - it redirect us to current week
     path('', views.dashboard, name='dashboard'),
