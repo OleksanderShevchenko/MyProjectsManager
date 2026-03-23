75f294c1eda1748ac3f47f01c08481308136ecf5
Oleksander  Shevchenko <oleksandr.shevchenko@damen.com>
2026-03-23 18:52:25 +0200

    Add models of imported data into admin panel

--- msproject_import/admin.py
+++ msproject_import/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ImportBatch, StagingProject, StagingLog
+
+class StagingProjectInline(admin.TabularInline):
+    model = StagingProject
+    extra = 0
+
+class StagingLogInline(admin.TabularInline):
+    model = StagingLog
+    extra = 0
+
+@admin.register(ImportBatch)
+class ImportBatchAdmin(admin.ModelAdmin):
+    list_display = ('user', 'year', 'status', 'created_at')
+    list_filter = ('status', 'year', 'user')
+    inlines = [StagingProjectInline, StagingLogInline]
