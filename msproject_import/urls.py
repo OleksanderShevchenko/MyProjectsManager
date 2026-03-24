3b4262fc66a4fc7f0252f07ec888f72da7f317ad
Oleksander  Shevchenko <oleksandr.shevchenko@damen.com>
2026-03-24 17:54:24 +0200

    Add urls for new views for pending imported projects for the past year and for their approval.

--- msproject_import/urls.py
+++ msproject_import/urls.py
@@ -7,4 +7,8 @@ app_name = 'msproject_import'
 urlpatterns = [
     path('start/', views.import_start, name='start'),
     path('mapping/<int:batch_id>/', views.import_mapping, name='mapping'),
+
+    # manager's urls
+    path('pending/', views.pending_imports, name='pending'),
+    path('approve/<int:batch_id>/', views.approve_import, name='approve'),
 ]
