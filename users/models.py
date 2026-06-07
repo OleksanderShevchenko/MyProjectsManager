d2558e75bae78d5c3311064101d70cca632fb368
Oleksander  Shevchenko <oleksandr.shevchenko@damen.com>
2026-06-07 19:01:57 +0300

    Add property that defines for a user if a user is manager for any active projects.

--- users/models.py
+++ users/models.py
@@ -3,6 +3,11 @@ from django.db import models
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    # Here we will add new fields for a user
-    # for example: bio = models.TextField(blank=True)
-    pass
+
+    @property
+    def is_active_manager(self) -> bool:
+        """Checks if the user is a manager of at least one open project"""
+        # managed_projects field comes from Reverse Relations "magic" of django
+        # that creates this field in "work_time_reporter/models.py" in Project class
+        # when we define manager field
+        return self.managed_projects.filter(is_active=True).exists()
