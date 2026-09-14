20ca132ce9c6f47d1ac22fcda16317ef391ce907
Oleksander Shevchenko <oleksander.shevchenko777@gmail.com>
2026-09-14 11:44:07 +0300

    feat: register django_htmx app and HtmxMiddleware in Django settings

--- config/settings.py
+++ config/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Third-party apps
+    'django_htmx',
+
     # first local app
     'users',
     'work_time_reporter',
@@ -60,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_htmx.middleware.HtmxMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
