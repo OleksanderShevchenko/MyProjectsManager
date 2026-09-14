36ec8d1d64b888a6076856b2c690d95017865d16
Oleksander  Shevchenko <oleksandr.shevchenko@damen.com>
2026-09-14 10:48:10 +0300

    docs: restore explanatory comment for managed_projects reverse relation in CustomUser

--- users/models.py
+++ users/models.py
@@ -6,6 +6,9 @@ class CustomUser(AbstractUser):
     @property
     def is_active_manager(self) -> bool:
         """Checks if the user is a manager of at least one open project or in Project Manager group"""
+        # managed_projects field comes from Reverse Relations "magic" of django
+        # that creates this field in "work_time_reporter/models.py" in Project class
+        # when we define manager field
         return (
             self.managed_projects.filter(is_active=True).exists()
             or self.groups.filter(name='Project Manager').exists()
